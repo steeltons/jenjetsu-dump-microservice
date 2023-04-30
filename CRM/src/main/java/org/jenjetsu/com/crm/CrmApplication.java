fb9def587868429a8b883df3971f26bfa2e7fedb
Lakmilak <Tsvetstas30@gmail.com>
2023-04-30 18:53:16 +1000

    added @EnableCache annotation

--- CRM/src/main/java/org/jenjetsu/com/crm/CrmApplication.java
+++ CRM/src/main/java/org/jenjetsu/com/crm/CrmApplication.java
@@ -2,11 +2,13 @@ package org.jenjetsu.com.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"org.jenjetsu.com.core", "org.jenjetsu.com.crm"})
 @EnableEurekaServer
+@EnableCaching
 public class CrmApplication {
 
     public static void main(String[] args) {
